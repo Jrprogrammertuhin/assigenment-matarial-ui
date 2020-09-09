40a3a69e134d5060f4fdd5ee996653019d6646d6
Jrprogrammertuhin <67548516+Jrprogrammertuhin@users.noreply.github.com>
2020-09-09 20:27:01 +0800

    assigenment-matarial-ui finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MediaCard from './Component/Card/MediaCard';
+import LearnMore from './Component/Button/LearnMore/LearnMore';
 
 function App() {
+  const [post , setPost] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => setPost(data))
+  }, [])
+  // const [lernMore , setLearnMore] = useState([]);
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/comments')
+  //   .then(response => response.json())
+  //   .then(data => setLearnMore(data))
+  // }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        post.map(post => <MediaCard post={post}></MediaCard>)
+      }
+      {
+        // lernMore.map(lernMore => <LearnMore lernMore={lernMore}></LearnMore>)
+      }
     </div>
+    
   );
 }
 
---
+++ src/Component/Button/LearnMore/LearnMore.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LearnMore = (props) => {
+    console.log(props.LearnMore.email)
+    // const {name} = props.LearnMore;
+    return (
+        <div>
+            {/* <h1>Name:{name}</h1> */}
+        </div>
+    );
+};
+
+export default LearnMore;
